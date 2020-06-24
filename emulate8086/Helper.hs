580772a7d32f2b42176a00443ddff612d8b46278
waddlaw <ingronze@gmail.com>
2020-06-24 19:04:06 +0900

    wip

--- emulate8086/Helper.hs
+++ emulate8086/Helper.hs
@@ -1,29 +1,31 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Helper where
 
-import Numeric
-import Data.Word
 import Data.Bits
+import Data.Word
+import Numeric
 
 instance Num Bool where
-    (+) = xor
-    (-) = xor
-    (*) = (&&)
-    abs = id
-    signum = id
-    fromInteger = odd
+  (+) = xor
+  (-) = xor
+  (*) = (&&)
+  abs = id
+  signum = id
+  fromInteger = odd
 
 instance Real Bool where
-    toRational = toRational . fromEnum
+  toRational = toRational . fromEnum
 
 instance Integral Bool where
-    toInteger = toInteger . fromEnum
-    a `quotRem` 1 = (a, 0)
-    a `quotRem` 0 = error $ "quotRem " ++ show a ++ " 0 :: Bool"
+  toInteger = toInteger . fromEnum
+  a `quotRem` 1 = (a, 0)
+  a `quotRem` _ = error $ "quotRem " ++ show a ++ " 0 :: Bool"
 
 ----------------------------------------------
 
 {-# INLINE debug #-}
+debug :: Bool
 debug = False
 
 showHex' :: (Show a, Integral a) => Int -> a -> String
@@ -31,4 +33,3 @@ showHex' i x = replicate (i - length s) '0' ++ s where s = showHex x ""
 
 segAddr :: Word16 -> Word16 -> Int
 segAddr s w = fromIntegral s `shiftL` 4 + fromIntegral w
-
--- emulate8086/Main.hs
+++ emulate8086/Main.hs
@@ -1,50 +1,51 @@
-import qualified Data.ByteString as BS
 import Control.Concurrent
-import System.IO
-import Sound.ALUT
-
+import qualified Data.ByteString as BS
 import Dos
 import Emulate
 import GUI
 import MachineState
+import Sound.ALUT
+import System.IO
 
 --------------------------------------------------------------------------------
 
+gameFile :: [Char]
 gameFile = "restunts/stunts/game.exe"
+
+loadSegment :: Num p => p
 loadSegment = 0x20e
 
+main :: IO ()
 main = withProgNameAndArgs runALUT $ \_ args -> do
-    let cycles = case args of
-            [n] -> read n
-            _ -> 1000000000  -- hack
+  let cycles = case args of
+        [n] -> read n
+        _ -> 1000000000 -- hack
+  bData <- createBufferData (Square 440 0 0.1)
+  buff <- createBuff bData 1
+  [source] <- genObjectNames 1
+  loopingMode source $= Looping
+  stop [source]
+  buffer source $= Just buff
 
-    bData <- createBufferData (Square 440 0 0.1)
-    buff <- createBuff bData 1
-    [source] <- genObjectNames 1
-    loopingMode source $= Looping
-    stop [source]
-    buffer source $= Just buff
-
-    hSetBuffering stdout NoBuffering
-    let addChange m = modifyMVar_ changeState $ \n -> return $ n >> m
---    l <- getLabels
-    tid <- myThreadId
-    game <- BS.readFile gameFile
-    ir <- use'' interruptRequest
-    _ <- forkIO $ do
-        verboseLevel ...= 1 
-        soundSource ...= source
-        getInst <- loadExe loadSegment game
-        loadCache getInst
-        _ <- forkIO timerThread
-        let cyc = mkStep >>= checkInt cycles cyc
-        cyc
-        killThread tid
-    drawWithFrameBuffer addChange (\r -> modifyMVar_ ir $ return . (++[r])) $ return ()
+  hSetBuffering stdout NoBuffering
+  let addChange m = modifyMVar_ changeState $ \n -> return $ n >> m
+  --    l <- getLabels
+  tid <- myThreadId
+  game <- BS.readFile gameFile
+  ir <- use'' interruptRequest
+  _ <- forkIO $ do
+    verboseLevel ...= 1
+    soundSource ...= source
+    getInst <- loadExe loadSegment game
+    loadCache getInst
+    _ <- forkIO timerThread
+    let cyc = mkStep >>= checkInt cycles cyc
+    cyc
+    killThread tid
+  drawWithFrameBuffer addChange (\r -> modifyMVar_ ir $ return . (++ [r])) $ return ()
 
 createBuff :: BufferData a -> Frequency -> IO Buffer
 createBuff (BufferData m fmt f) x = do
-    [b] <- genObjectNames 1
-    bufferData b $= BufferData m fmt (x*f)
-    return b
-
+  [b] <- genObjectNames 1
+  bufferData b $= BufferData m fmt (x * f)
+  return b
